55bc5bcd2572c7dae06bb6cad5c932fa8ed4baf2
eyecbul-oss <eyecbul@gmail.com>
2026-05-09 15:04:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sezr-matematik-v16";
+const CACHE_NAME = "sezr-matematik-v17";
 
 const FILES_TO_CACHE = [
   "./",
@@ -10,6 +10,7 @@ const FILES_TO_CACHE = [
   "./preview.jpg",
   "./profil.jpg",
   "./math-bg.jpg",
+  "./focus-room.png",
   "./lofi.mp3"
 ];
 
