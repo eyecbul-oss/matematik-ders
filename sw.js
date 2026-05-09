8da6c313277caea50f8c1cd9d558dfb05f49869c
eyecbul-oss <eyecbul@gmail.com>
2026-05-09 12:49:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,63 +1,79 @@
-const CACHE_NAME = "sezr-matematik-v7";
+// sw.js
+
+const CACHE_NAME = "sezr-matematik-v10";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./offline.html",
-  "./manifest.json",
-  "./favicon.png",
-  "./favicon.ico",
-  "./preview.jpg",
-  "./profil.jpg",
-  "./math-bg.jpg"
+"./",
+"./index.html",
+"./favicon.png",
+"./favicon.ico",
+"./preview.jpg",
+"./profil.jpg",
+"./math-bg.jpg"
 ];
 
 self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(FILES_TO_CACHE).catch(function () {
-        return Promise.resolve();
-      });
-    })
-  );
-  self.skipWaiting();
+
+event.waitUntil(
+
+caches.open(CACHE_NAME).then(function (cache) {
+
+return cache.addAll(FILES_TO_CACHE);
+
+})
+
+);
+
+self.skipWaiting();
+
 });
 
 self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(
-        keys.map(function (key) {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
+
+event.waitUntil(
+
+caches.keys().then(function (keys) {
+
+return Promise.all(
+
+keys.map(function (key) {
+
+if (key !== CACHE_NAME) {
+
+return caches.delete(key);
+
+}
+
+})
+
+);
+
+})
+
+);
+
+self.clients.claim();
+
 });
 
 self.addEventListener("fetch", function (event) {
-  if (event.request.method !== "GET") return;
 
-  event.respondWith(
-    fetch(event.request)
-      .then(function (networkResponse) {
-        const responseClone = networkResponse.clone();
-        caches.open(CACHE_NAME).then(function (cache) {
-          cache.put(event.request, responseClone).catch(function(){});
-        });
-        return networkResponse;
-      })
-      .catch(function () {
-        return caches.match(event.request).then(function (cachedResponse) {
-          if (cachedResponse) return cachedResponse;
+if (event.request.method !== "GET") return;
 
-          if (event.request.mode === "navigate") {
-            return caches.match("./offline.html");
-          }
-        });
-      })
-  );
+event.respondWith(
+
+caches.match(event.request).then(function (cachedResponse) {
+
+if (cachedResponse) {
+
+return cachedResponse;
+
+}
+
+return fetch(event.request);
+
+})
+
+);
+
 });
