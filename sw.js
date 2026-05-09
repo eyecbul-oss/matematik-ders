5d3689d2a2d5112645c513528784640e5bcc7e45
eyecbul-oss <eyecbul@gmail.com>
2026-05-09 13:14:13 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,79 +1,63 @@
-// sw.js
-
-const CACHE_NAME = "sezr-matematik-v10";
+const CACHE_NAME = "sezr-matematik-v12";
 
 const FILES_TO_CACHE = [
-"./",
-"./index.html",
-"./favicon.png",
-"./favicon.ico",
-"./preview.jpg",
-"./profil.jpg",
-"./math-bg.jpg"
+  "./",
+  "./index.html",
+  "./offline.html",
+  "./manifest.json",
+  "./favicon.png",
+  "./favicon.ico",
+  "./preview.jpg",
+  "./profil.jpg",
+  "./math-bg.jpg"
 ];
 
 self.addEventListener("install", function (event) {
-
-event.waitUntil(
-
-caches.open(CACHE_NAME).then(function (cache) {
-
-return cache.addAll(FILES_TO_CACHE);
-
-})
-
-);
-
-self.skipWaiting();
-
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE).catch(function () {
+        return Promise.resolve();
+      });
+    })
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", function (event) {
-
-event.waitUntil(
-
-caches.keys().then(function (keys) {
-
-return Promise.all(
-
-keys.map(function (key) {
-
-if (key !== CACHE_NAME) {
-
-return caches.delete(key);
-
-}
-
-})
-
-);
-
-})
-
-);
-
-self.clients.claim();
-
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.map(function (key) {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
 
-if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then(function (networkResponse) {
+        const responseClone = networkResponse.clone();
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseClone).catch(function(){});
+        });
+        return networkResponse;
+      })
+      .catch(function () {
+        return caches.match(event.request).then(function (cachedResponse) {
+          if (cachedResponse) return cachedResponse;
 
-event.respondWith(
-
-caches.match(event.request).then(function (cachedResponse) {
-
-if (cachedResponse) {
-
-return cachedResponse;
-
-}
-
-return fetch(event.request);
-
-})
-
-);
-
+          if (event.request.mode === "navigate") {
+            return caches.match("./offline.html");
+          }
+        });
+      })
+  );
 });
